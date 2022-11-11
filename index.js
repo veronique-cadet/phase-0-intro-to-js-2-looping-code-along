90420c9fc53530b50a88c640488081af9f1325f4
Veronique Cadet <cadetvero@gmail.com>
2022-11-11 19:09:40 +0100

    commit tree 14e2ddab1d852a75dd8f3dd464ec4335b9e83a43

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(
+      "Thank you, " + names[i] + "," + " for the wonderful " + event + " gift!"
+    );
+  }
+  return messages;
+}
+
+function countDown() {
+  let countDown = 0;
+  while (countDown < 11) {
+    console.log(countDown++);
+  }
+}
